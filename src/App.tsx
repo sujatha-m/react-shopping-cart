319cb13cb9b91711bee8ff226af6869d8db383e5
sujatha <jhanavi.bhushan@gmail.com>
2021-02-18 19:04:11 -0500

    Created funcion to get data from fakestore api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+//components
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+//styles
+import { Wrapper } from './App.styles';
+//Types
+export type CartItemType = {
+  id : number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> => 
+   await (await fetch ('https://fakestoreapi.com/products')).json();
 
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products', 
+    getProducts
+    );
+    console.log(data);
+
+    const getTotalItems = () => null;
+
+    const handleAddToCart = () => null;
+
+    const handleRemoveFromCart = () => null;
+
+    if(isLoading) return <LinearProgress />;
+    if(error) return <div>Something went wrong ...</div>
   return (
     <div className="App">
       Start
