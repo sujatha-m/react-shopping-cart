3a4bf52cf866d2d6a665f7e9e15bd221133cff73
sujatha <jhanavi.bhushan@gmail.com>
2021-02-19 15:17:27 -0500

    Functionality added to the cart to remove items and to add items

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -11,14 +11,22 @@ type Props = {
 };
 
 const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart}) => {
-    
+    const calculateTotal = (items: CartItemType[]) =>
+      items.reduce((ack: number, item) => ack + item.amount * item.price, 0)
+
     return(
         <Wrapper>
             <h2>Your Shoppint Cart</h2>
             {cartItems.length === 0 ? <p>No items in cart.</p> : null}
             {cartItems.map(item =>(
-                <CartItem />
+                <CartItem 
+                   key = {item.id}
+                   item = {item}
+                   addToCart = {addToCart}
+                   removeFromCart = {removeFromCart}
+                />
             ))}
+            <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
         </Wrapper>
     )
 }
