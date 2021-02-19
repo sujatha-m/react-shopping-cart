70fcfbf27140e4b89391653cfb3f9b72b8dcfbe4
sujatha <jhanavi.bhushan@gmail.com>
2021-02-19 13:20:45 -0500

    Added styles to the items

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 //components
+import Item from './Item/Item';
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
@@ -31,16 +32,22 @@ const App = () => {
 
     const getTotalItems = () => null;
 
-    const handleAddToCart = () => null;
+    const handleAddToCart = (clickedItem: CartItemType) => null;
 
     const handleRemoveFromCart = () => null;
 
     if(isLoading) return <LinearProgress />;
     if(error) return <div>Something went wrong ...</div>
   return (
-    <div className="App">
-      Start
-    </div>
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+           <Grid item key={item.id} xs={12} sm={4}>
+             <Item item={item} handleAddToCart={handleAddToCart} />
+           </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
